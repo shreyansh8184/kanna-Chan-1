4a17398363021229222f34859d5ac46be058a674
shreyansh000 <55754035+shreyansh000@users.noreply.github.com>
2020-02-26 00:53:08 +0530

    Add files via upload

---
+++ haruka/modules/boobs.py
@@ -0,0 +1,26 @@
+from telegram import ChatAction
+from haruka.modules.helper_funcs.filters import CustomFilters
+import html
+import urllib.request
+import re
+import json
+from typing import Optional, List
+import time
+import urllib
+from urllib.request import urlopen, urlretrieve
+from urllib.parse import quote_plus, urlencode
+import requests
+from telegram import Message, Chat, Update, Bot, MessageEntity
+from telegram import ParseMode
+from telegram.ext import CommandHandler, run_async, Filters
+from haruka import dispatcher
+from haruka.__main__ import STATS, USER_INFO
+from haruka.modules.disable import DisableAbleCommandHandler
+
+def boobs(bot: Bot, update: Update):
+    nsfw = requests.get('http://api.oboobs.ru/noise/1').json()[0]["preview"]
+    final = "http://media.oboobs.ru/{}".format(nsfw)
+    update.message.reply_photo(final)
+		
+BOOBS_HANDLER = DisableAbleCommandHandler("boobs", boobs, filters=CustomFilters.sudo_filter)
+dispatcher.add_handler(BOOBS_HANDLER)
