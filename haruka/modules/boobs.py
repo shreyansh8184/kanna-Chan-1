b14253e8b19e4ead7c552df48cf3e5f9c223f4ad
shreyansh000 <55754035+shreyansh000@users.noreply.github.com>
2020-02-28 13:24:14 +0530

    Update boobs.py

--- haruka/modules/boobs.py
+++ haruka/modules/boobs.py
@@ -21,6 +21,13 @@ def boobs(bot: Bot, update: Update):
     nsfw = requests.get('http://api.oboobs.ru/noise/1').json()[0]["preview"]
     final = "http://media.oboobs.ru/{}".format(nsfw)
     update.message.reply_photo(final)
+
+def khiladi(bot: Bot, update: Update):
+    final1 = "https://telegra.ph/file/051227609674fc788d030.jpg"
+    update.message.reply_photo(final1)
 		
 BOOBS_HANDLER = DisableAbleCommandHandler("boobs", boobs, filters=CustomFilters.sudo_filter)
+KHILADI_HANDLER = DisableAbleCommandHandler("khiladi", khiladi)
+
 dispatcher.add_handler(BOOBS_HANDLER)
+dispatcher.add_handler(KHILADI_HANDLER)
