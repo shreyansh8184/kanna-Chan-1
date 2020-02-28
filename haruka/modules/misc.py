36b0f80ecd4e6dc8e674bc5a35fbe7ac07c05a8a
shreyansh000 <55754035+shreyansh000@users.noreply.github.com>
2020-02-28 18:27:17 +0530

    Update misc.py

--- haruka/modules/misc.py
+++ haruka/modules/misc.py
@@ -125,7 +125,7 @@ def get_id(bot: Bot, update: Update, args: List[str]):
 
         else:
             
-            update.effective_message.reply_text(tld(chat.id, "Your id: `{}`\nChat id: {}").format(user.id, chat.id),
+            update.effective_message.reply_text(tld(chat.id, "Chat id: {}").format(chat.id),
                                                 parse_mode=ParseMode.MARKDOWN)
 
 
