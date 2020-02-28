04b64b4236a01e82489d4c0a424f709139c4d16a
shreyansh000 <55754035+shreyansh000@users.noreply.github.com>
2020-02-29 00:20:18 +0530

    Update misc.py

--- haruka/modules/misc.py
+++ haruka/modules/misc.py
@@ -34,6 +34,7 @@ from haruka.modules.translations.strings import tld
 
 from requests import get
 
+user_id1 = extract_user(update.effective_message, args)
 
 @run_async
 def insults(bot: Bot, update: Update):
@@ -121,7 +122,7 @@ def get_id(bot: Bot, update: Update, args: List[str]):
 
                                                         
     else:
-        user_id1 = extract_user(update.effective_message, args)
+        
         update.effective_message.reply_text(tld(chat.id, f"Your id: `{user_id1}` \nChat id: `{}`").format(chat.id),
                                             parse_mode=ParseMode.MARKDOWN)
 
