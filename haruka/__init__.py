f58a73569481eb816e2fab2c541f5b912b3ad948
shreyansh000 <55754035+shreyansh000@users.noreply.github.com>
2020-02-29 21:11:32 +0530

    Update __init__.py

--- haruka/__init__.py
+++ haruka/__init__.py
@@ -60,7 +60,7 @@ if ENV:
     STRICT_GBAN = bool(os.environ.get('STRICT_GBAN', False))
     TEMPORARY_DATA = os.environ.get('TEMPORARY_DATA', None)
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
-    spamfilters = int(os.environ.get('spamfilters', None))
+    spamfilters = int(os.environ.get('spamfilters'))
     
 else:
     from haruka.config import Development as Config
