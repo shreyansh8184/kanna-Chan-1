32a3737388edf73e19c1828556fb6ed53706649b
shreyansh000 <55754035+shreyansh000@users.noreply.github.com>
2020-03-02 17:43:05 +0530

    Update boobs.py

--- haruka/modules/boobs.py
+++ haruka/modules/boobs.py
@@ -4,6 +4,7 @@ import html
 import urllib.request
 import re
 import json
+import random
 from typing import Optional, List
 import time
 import urllib
@@ -25,9 +26,20 @@ def boobs(bot: Bot, update: Update):
 def khiladi(bot: Bot, update: Update):
     final1 = "https://telegra.ph/file/051227609674fc788d030.jpg"
     update.message.reply_photo(final1)
+
+def avn(bot: Bot, update: Update):
+    avengers = (
+      "https://telegra.ph//file/8201c6a5363061b4f0196.jpg"
+      "https://telegra.ph//file/8a525ec13114d1fa62e5d.jpg"
+      "https://telegra.ph//file/ff0a5befc03599643be59.jpg"
+    )
+    avn1 = random.choice(avn1)
+    update.message.reply_photo(avn)
 		
 BOOBS_HANDLER = DisableAbleCommandHandler("boobs", boobs, filters=CustomFilters.sudo_filter)
 KHILADI_HANDLER = DisableAbleCommandHandler("khiladi", khiladi)
+AVN_HANDLER = DisableAbleCommandHandler("avn", avn)
 
-dispatcher.add_handler(BOOBS_HANDLER)
+dispatcher.add_handler(AVN_HANDLER)
+diapatcher.add_handler(BOOBS_HANDLER)
 dispatcher.add_handler(KHILADI_HANDLER)
