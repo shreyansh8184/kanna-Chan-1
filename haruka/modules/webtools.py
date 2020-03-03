d99dafe558c3891e52d411c689509ef9f635292d
shreyansh000 <55754035+shreyansh000@users.noreply.github.com>
2020-03-03 20:58:31 +0530

    Update webtools.py

--- haruka/modules/webtools.py
+++ haruka/modules/webtools.py
@@ -72,6 +72,8 @@ def rtt(bot: Bot, update: Update):
 @run_async
 def ping(bot: Bot, update: Update):
     test = speedtest.Speedtest()
+    test.get_best_server()
+    test.download()
     test.results.share()
     result = test.results.dict()
     update.effective_message.reply_text("🏓Pong! \n⏰Reply Took: {}").format(result['ping'])
