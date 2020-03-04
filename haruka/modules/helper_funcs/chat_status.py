73abaec02c76b900ea7e302ae9bb536007f3bb88
shreyansh000 <55754035+shreyansh000@users.noreply.github.com>
2020-03-04 17:19:12 +0530

    Update chat_status.py

--- haruka/modules/helper_funcs/chat_status.py
+++ haruka/modules/helper_funcs/chat_status.py
@@ -113,7 +113,6 @@ def user_admin(func):
     @wraps(func)
     def is_admin(bot: Bot, update: Update, *args, **kwargs):
         user = update.effective_user  # type: Optional[User]
-        chat = update.effective_chat  # type: Optional[Chat]
         if user and is_user_admin(update.effective_chat, user.id):
             return func(bot, update, *args, **kwargs)
 
@@ -123,8 +122,8 @@ def user_admin(func):
         elif DEL_CMDS and " " not in update.effective_message.text:
             update.effective_message.delete()
 
-        elif (admin_sql.command_reaction(chat.id) == True):
-            update.effective_message.reply_text("Who dis non-admin telling me what to do?")
+        else:
+            update.effective_message.reply_text(languages.tl(update.effective_message, "Who is this non admin telling me what to do?"))
 
     return is_admin
 
