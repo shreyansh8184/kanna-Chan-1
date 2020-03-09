e9115af30335c3b8362067883d5998bba43945ca
shreyansh000 <55754035+shreyansh000@users.noreply.github.com>
2020-03-09 10:11:30 +0530

    Update msg_deleting.py

--- haruka/modules/msg_deleting.py
+++ haruka/modules/msg_deleting.py
@@ -53,7 +53,7 @@ def purge(bot: Bot, update: Update, args: List[str]) -> str:
 
             purge = bot.send_message(chat.id, tld(chat.id, "Purge complete."))
             messageId = purge['message_id']
-            bot.delete_message((chat_id, message_id))
+            bot.delete_message((chat.id, message_id))
             return "<b>{}:</b>" \
                    "\n#PURGE" \
                    "\n<b>Admin:</b> {}" \
